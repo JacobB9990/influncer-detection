278bd9c2a86696c8469f50b3b387dfe93876516f
JacobB9990 <jacobb9990@gmail.com>
2025-07-24 23:46:43 -0400

    Fixed the typing mistakes in maximizing-the-spread-of-influence-through-a-social-network

--- maximizing-the-spread-of-influence-through-a-social-network/page_rank.py
+++ maximizing-the-spread-of-influence-through-a-social-network/page_rank.py
@@ -1,26 +1,24 @@
 import networkx as nx
 import random
-from typing import List, Set
+from typing import List, Set, Optional
 
 
-def generate_graph(
-    n: int = 100, m: int = 2
-) -> nx.DiGraph:  # This is here just incase/running independently
+def generate_graph(n: int = 100, m: int = 2) -> nx.DiGraph:
     G = nx.barabasi_albert_graph(n, m)
     directed_G = G.to_directed()
 
     for node in directed_G.nodes():
         in_edges = list(directed_G.in_edges(node))
         total = len(in_edges)
-        for u, v in in_edges:
+        for source_node, target_node in in_edges:
             if total > 0:
-                directed_G[u][v]["weight"] = 1 / total
+                directed_G[source_node][target_node]["weight"] = 1 / total
             else:
-                directed_G[u][v]["weight"] = 0.0
+                directed_G[source_node][target_node]["weight"] = 0.0
 
     return directed_G
 
-def run_ic(G: nx.DiGraph, seeds: List[int]) -> Set[int]:
+def run_ic(G: nx.DiGraph, seeds: Set[int]) -> Set[int]:
     active = set(seeds)
     newly_active = set(seeds)
 
@@ -36,7 +34,7 @@ def run_ic(G: nx.DiGraph, seeds: List[int]) -> Set[int]:
         active.update(newly_active)
     return active
 
-def estimate_spread(G: nx.DiGraph, seeds: List[int], simulations: int = 1000) -> float:
+def estimate_spread(G: nx.DiGraph, seeds: Set[int], simulations: int = 1000) -> float:
     total_spread = 0
     for _ in range(simulations):
         active = run_ic(G, seeds)
@@ -46,12 +44,12 @@ def estimate_spread(G: nx.DiGraph, seeds: List[int], simulations: int = 1000) ->
 
 def top_pagerank_nodes(G: nx.DiGraph, k: int) -> Set[int]:
     pr = nx.pagerank(G, alpha=0.85)
-    top_k = sorted(pr, key=pr.get, reverse=True)[:k]
+    top_k = [node for node, _ in sorted(pr.items(), key=lambda x: x[1], reverse=True)[:k]]
 
-    return top_k
+    return set(top_k)
 
 
-def run_simulation(n: int = 100, m: int = 2, k: int = 4, simulations: int = 1000 , G: nx.DiGraph = None) -> None:
+def run_simulation(n: int = 100, m: int = 2, k: int = 4, simulations: int = 1000 , G: Optional[nx.DiGraph] = None) -> None:
     if G is None:
         G = generate_graph(n, m)
 
