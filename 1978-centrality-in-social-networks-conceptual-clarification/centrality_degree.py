954a609f3d9fc78b813ea21660ead825f5071acc
JacobB9990 <jacobb9990@gmail.com>
2025-08-28 10:07:05 -0400

    Renamed files so its in order. Also added 1998 pagerank. Some other minor fixes.

---
+++ 1978-centrality-in-social-networks-conceptual-clarification/centrality_degree.py
@@ -0,0 +1,47 @@
+# Goal: measure degree centrality.
+
+from typing import Dict, List
+
+Graph = Dict[str, List[str]]
+
+G_star: Graph = {
+    'A': ['B', 'C', 'D', 'E'],
+    'B': ['A'],
+    'C': ['A'],
+    'D': ['A'],
+    'E': ['A']
+}
+
+G_chain: Graph = {
+    'A': ['B'],
+    'B': ['A', 'C'],
+    'C': ['B', 'D'],
+    'D': ['C', 'E'],
+    'E': ['D'],
+}
+
+G_clique: Graph = {
+    'A': ['B', 'C', 'D'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D'],
+    'D': ['A', 'B', 'C'],
+}
+
+def degree_centrality(graph: Graph, normalize: bool = False) -> Dict[str, float]:
+    n = len(graph)
+    centrality = {}
+    for node in graph:
+        degree: int = len(graph[node])
+        if normalize:
+            degree_norm = degree / (n - 1)
+            centrality[node] = degree_norm
+        else:
+            centrality[node] = degree
+    return centrality
+
+print("Degree Centrality Results:")
+for name, G in [('Star', G_star), ('Chain', G_chain), ('Clique', G_clique)]:
+    print(f'\n{name} graph:')
+    results = degree_centrality(G, normalize=False)
+    for node, centrality in results.items():
+        print(f'Node {node}: {centrality:.2f}')
---
+++ 1978-centrality-in-social-networks-conceptual-clarification/test_graphs/clique_graph.md
@@ -0,0 +1,21 @@
+**Connected graph with a central node (A) connected to all other nodes (B, C, D, E):**
+
+```py
+A - B
+| X |
+C - D
+
+graph = {
+    'A': ['B', 'C', 'D'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A', 'B', 'D'],
+    'D': ['A', 'B', 'C']
+}
+```
+
+| Node | Degree (norm) | Degree (raw) | Closeness (norm) | Betweenness (norm) | Betweenness (raw) |
+| ---- | ------------- | ------------ | ---------------- | ------------------ | ----------------- |
+| A    | 1.00          | 3.00         | 1.00             | 0.00               | 0.00              |
+| B    | 1.00          | 3.00         | 1.00             | 0.00               | 0.00              |
+| C    | 1.00          | 3.00         | 1.00             | 0.00               | 0.00              |
+| D    | 1.00          | 3.00         | 1.00             | 0.00               | 0.00              |
