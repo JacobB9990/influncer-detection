8cc918360ea39fc8819971443dd3398358a4db5e
JacobB9990 <jacobb9990@gmail.com>
2025-10-14 15:06:46 -0400

    Moving to PC

---
+++ 2005-influential-nodes-in-a-diffusion-model-for-social-networks/Diffusion_model.py
@@ -0,0 +1,10 @@
+import networkx as nx
+from typing import Dict
+
+Graph = nx.Graph
+
+def generate_graph():
+    pass
+
+def general_threshold_model(G: Graph, seeds: set[int], activation_functions, max_steps: int = 100):
+    pass
